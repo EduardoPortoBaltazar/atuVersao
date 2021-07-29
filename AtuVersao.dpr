d0c3dcf21b6bbf351b84855c1f0874f3572850b4
eduardoportobaltazar <eduardo.porto.baltazar@gmail.com>
2021-07-29 19:28:20 -0300

    Criação do Projeto

---
+++ AtuVersao.dpr
@@ -0,0 +1,14 @@
+program AtuVersao;
+
+uses
+  Vcl.Forms,
+  FAtuVersao in 'Source\FAtuVersao.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
